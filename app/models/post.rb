64b16cd1e27ca0e1b7d909fc7e999299bf56e224
Tatiana <Bowmantatiana2@gmail.com>
2023-02-22 14:15:32 -0800

    commit tree cbe48caaf5518450e44f87ddcd94f03fc737b159

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+    validate :only_click_bait_titles
+
+    ARR = [/Won't Believe/i, /Secret/i, /Top \d/i,/Guess/i]
+    def only_click_bait_titles
+        if ARR.none? {|pattern| pattern.match title}
+            errors.add(:title, "needs a better title")
+         end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "seeding data..."
+
+puts "seeding author data..."
+# author data
+Author.create(name:"Tatiana" , phone_number:"215-960-6468")
+Author.create(name:"Peachy" , phone_number:"215-960-6999")
+Author.create(name:"Olivia", phone_number:"215-970-6468")
+
+puts "seeding post data..."
+#post data
+Post.create(title:"Coming to ohio" , content: "travel" , summary: "always wanting to lve in ohio", category:"travel")
+Post.create(title:"Staying in ohio" , content:"day in the life" , summary:"moving to ohio", category: "travel")
+puts "almost done!"
+Post.create(title:"bad time in ohio" , content:"bad day in the life" , summary:"hating the life in ohio", category:"travel")
+Post.create(title:"Leaving ohio" , content: "moving", summary:"moving to a new state from ohio", category:"travel")
+puts "data seeded!"
